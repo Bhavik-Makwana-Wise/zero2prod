b9d77426859e5aa6bbc134a62468bcbfd0753c22
Bhavik Makwana <bhavik.makwana@transferwise.com>
2022-03-31 15:05:22 +0100

    Add integration test
    refactored code to run as binary and a library
    refactored code to not use async for run function
    add test for health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+   run()?.await
 }
