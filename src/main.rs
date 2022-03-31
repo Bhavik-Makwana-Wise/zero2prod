56bc6e477b535b60c3b90c01420b52782bf64632
Bhavik Makwana <bhavik.makwana@transferwise.com>
2022-03-31 14:39:46 +0100

    Add health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
