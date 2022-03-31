ecc13434779a259d07ae18271e28bf2d6dfbb5d6
Bhavik Makwana <bhavik.makwana@transferwise.com>
2022-03-31 17:44:14 +0100

    Add sqlx integration and db

---
+++ scripts/init_db.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -x
+
+set -eo pipefail
+
+if ! [ -x "$(command -v /usr/local/opt/libpq/bin/psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed"
+  echo >&2 "Install with:"
+  echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-deatures --features postgres"
+fi
+
+# Check if a custoom user has been set, o/w default to 'postgres'
+DB_USER=${POSTGRES_USER:=postgres}
+# Check if a custom password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a custom db name has been set, o/w default to 'newsletter'
+DB_NAME="${POSTGRES_DB:=newsletter}"
+
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+# launch postgres using Docke
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
+
+# ping postgres until its ready
+export PGPASSWORD="${DB_PASSWORD}"
+until /usr/local/opt/libpq/bin/psql  -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable, sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
