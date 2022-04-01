e725a4351db6ad476999a527e7cb84ee37e64882
Bhavik Makwana <bhavik.makwana@transferwise.com>
2022-04-01 08:44:38 +0100

    Use configuration file to establish connection in main and in test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -21,6 +23,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to get configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres");
     let client = reqwest::Client::new();
 
     let body = "name=Johnny%20Bravo&email=Johnny_Bravo%40proton.com";
