1664d6320ed5476d643a6b3a67fc645d7893c281
Bhavik Makwana <bhavik.makwana@transferwise.com>
2022-04-10 12:10:08 +0100

    ADd ssl add env var injection update config

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -117,7 +117,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+        PgConnection::connect_with(&config.without_db())
             .await
             .expect("Failed to connect to Postgres");
     connection
@@ -126,7 +126,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to create database");
 
     // Migrate database
-    let db_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let db_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to postgres");
     sqlx::migrate!("./migrations")
